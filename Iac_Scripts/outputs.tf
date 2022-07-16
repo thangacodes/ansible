baec2da03a0f251667643175701a5efba1bb0190
send2durai <send2durai@hotmail.com>
2022-07-16 20:15:20 +0530

    initial commit

--- Iac_Scripts/outputs.tf
+++ Iac_Scripts/outputs.tf
@@ -1,6 +1,26 @@
 output "web_pubip" {
-  value = aws_instance.web.public_ip
+  value = "https://${aws_instance.web.public_ip}:8080/SampleWebApp/"
 }
 output "web_privip" {
-  value = aws_instance.web.private_ip
+  value = "https://${aws_instance.web.private_ip}:8080/SampleWebApp/"
+}
+
+output "user_id" {
+  value = aws_iam_user.assume_role_user.id
+}
+output "user_arn" {
+  value = aws_iam_user.assume_role_user.arn
+}
+
+output "secret_key" {
+  value     = aws_iam_access_key.user.secret
+  sensitive = true
+}
+
+output "access_key" {
+  value = aws_iam_access_key.user.id
+}
+
+output "vpc_id" {
+  value = "your ${local.Project}-${var.Environment}-vpc_id is ${aws_vpc.my_vpc.id}"
 }
