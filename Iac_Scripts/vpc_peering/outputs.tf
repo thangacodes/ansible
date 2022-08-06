ac8bf0bc94bb3454c61a04523e2b51364a9388a0
send2durai <send2durai@hotmail.com>
2022-08-06 09:44:50 +0530

    slight updates on outputs.tf

--- Iac_Scripts/vpc_peering/outputs.tf
+++ Iac_Scripts/vpc_peering/outputs.tf
@@ -10,3 +10,33 @@ output "second-vm-pubip" {
 output "second-vm-privip" {
   value = aws_instance.second-vm.private_ip
 }
+output "mumbai-vpcrange" {
+  value = aws_vpc.first.cidr_block
+}
+output "virginia-vpcrange" {
+  value = aws_vpc.second.cidr_block
+}
+output "mumbai-pubcidr" {
+  value = aws_subnet.first-pub-subnet.cidr_block
+}
+output "virginia-pubcidr" {
+  value = aws_subnet.second-pub-subnet.cidr_block
+}
+output "mumigw" {
+  value = aws_internet_gateway.first-pub-igw.id
+}
+output "virginiaigw" {
+  value = aws_internet_gateway.second-pub-igw.id
+}
+output "mum-sgp" {
+  value = aws_security_group.first-sgp.ingress
+}
+output "virginia-sgp" {
+  value = aws_security_group.second-sgp.ingress
+}
+output "mum-pubrt-routes" {
+  value = aws_route_table.first-pub-rt.route
+}
+output "virginia-pubrt-routes" {
+  value = aws_route_table.second-pub-rt.route
+}
