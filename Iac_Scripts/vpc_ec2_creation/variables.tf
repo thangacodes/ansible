477143d42560c58797bfc91a6e11a02676aa75ea
Thangadurai M <38905243+send2durai@users.noreply.github.com>
2022-10-18 22:33:18 +0530

    Update variables.tf

--- Iac_Scripts/vpc_ec2_creation/variables.tf
+++ Iac_Scripts/vpc_ec2_creation/variables.tf
@@ -55,9 +55,10 @@ variable "instance_type" {
   description = "By default it should t2.micro"
   type        = string
   default     = "t2.micro"
+}
 
   //validation {
     //condition     = can(regex("^[Tt][3].(large|xlarge|2xlarge)", var.instance_type))
     //error_message = "Invalid Instance Type name. You have to choose only - t3.large,t3.xlarge,t3.2xlarge?"
-  }
-}
+  //}
+//}
