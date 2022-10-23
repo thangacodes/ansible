a6abce067cfc90cb91b3ed3711df93aa069ea22b
Thangadurai M <38905243+send2durai@users.noreply.github.com>
2022-10-23 21:56:01 +0530

    Update ec2_ami.tf

--- Iac_Scripts/ec2_ami_creation/ec2_ami.tf
+++ Iac_Scripts/ec2_ami_creation/ec2_ami.tf
@@ -1,5 +1,5 @@
 resource "aws_ami_from_instance" "demo" {
   name               = "CICD-JENKINS-AMI"
-  source_instance_id = var.instance_id
+  source_instance_id = var.${INSTANCE_ID}
   tags               = var.tagging
 }
