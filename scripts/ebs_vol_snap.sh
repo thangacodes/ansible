cb8a64e11f65a6a08cd89bce22eabb2d66c2c536
Thangadurai M <38905243+send2durai@users.noreply.github.com>
2022-11-02 22:36:17 +0530

    Update ebs_vol_snap.sh

--- scripts/ebs_vol_snap.sh
+++ scripts/ebs_vol_snap.sh
@@ -3,5 +3,6 @@ echo "AWS CLI will be there to take EBS Volume snapshot"
 aws ec2 create-snapshot --volume-id $EBS_VOL_ID --description "This is my root volume snapshot" --region "ap-south-1"
 sleep 30
 aws ec2 describe-snapshots --owner-ids self  --query 'Snapshots[]' --region=ap-south-1 > snap.txt
-cat snap.txt | sed -n '11p'
+cat snap.txt | grep -i 'snap-i'
+cut -d ":" -f 2 snap.txt
 exit
