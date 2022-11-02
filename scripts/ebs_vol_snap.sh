3a6be26251da20415f40082a2039030125832b60
Thangadurai M <38905243+send2durai@users.noreply.github.com>
2022-11-02 19:26:04 +0530

    Update ebs_vol_snap.sh
    region is updated

--- scripts/ebs_vol_snap.sh
+++ scripts/ebs_vol_snap.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "AWS CLI will be there to take EBS Volume snapshot"
-aws ec2 create-snapshot --volume-id $EBS_VOL_ID --description "This is my root volume snapshot"
+aws ec2 create-snapshot --volume-id vol-1234567890abcdef0 --description "This is my root volume snapshot" --region "ap-south-1"
 exit
