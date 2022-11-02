48aec7aee20d3a61d5314ebfcb6882b86d32c0ab
Thangadurai M <38905243+send2durai@users.noreply.github.com>
2022-11-02 23:01:58 +0530

    Update ebs_vol_snap_del.sh

--- scripts/ebs_vol_snap_del.sh
+++ scripts/ebs_vol_snap_del.sh
@@ -2,6 +2,11 @@
 echo "Deleting EBS Volume snapshot using AWS CLI"
 cat snap.txt | sed -n '11p'
 cut -d ":" -f 2 snap.txt
-aws ec2 delete-snapshot --snapshot-id snap-1234567890abcdef0 --region "ap-south-1"
-sleep 30
+cut -d ":" -f 2 snap.txt | grep 'snap-0'
+cut -d ":" -f 2 snap.txt | grep 'snap-0' > snapid
+cat snapid
+echo $(cat snapid)
+sleep 5
+aws ec2 delete-snapshot --snapshot-id $(cat snapid) --region=ap-south-1
+sleep 20
 exit
